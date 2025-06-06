a10da508cbeb50cc5fcb4a3d07d2c85f371be483
jihanv <hedjmny@gmail.com>
2025-06-06 21:54:37 +0900

    Add dashboard and users again

---
+++ app/components/hello.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+
+const Hello = () => {
+
+    console.log("I am from the client.")
+
+    return (
+        <>
+            <div>Hello!</div>
+        </>
+
+    )
+}
+
+export default Hello
