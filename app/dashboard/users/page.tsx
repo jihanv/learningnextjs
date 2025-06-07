c2c14fb6c3b1b16260468fc07ea74c75bc9bbbe6
jihanv <hedjmny@gmail.com>
2025-06-07 20:57:45 +0900

    Add layouts

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page = () => {
     return (
         <>
-            <h1 className='text-3xl' >Dashboard User</h1>
+            <h1 className='text-3xl' >User</h1>
             <ul className='mt-10'>
                 <li><Link href="/dashboard/users/1">User 1</Link></li>
                 <li><Link href="/dashboard/users/2">User 2</Link></li>
